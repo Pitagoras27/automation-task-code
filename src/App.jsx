94a86d2b9471f1ec7cc4b7ee001e4d049f09078c
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2022-12-18 16:20:52 -0600

    feat: Prettier config added

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-
-import React from "react";
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App terst">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	const [count, setCount] = useState(0);
+	const name_4 = 'Vite';
+	return (
+		<div className="App terst">
+			<h1>{`${name_4} + React`}</h1>
+			<div className="card">
+				<button onClick={() => setCount(count => count + 1)}> {count}</button>
+				<p>
+					Edit <code>src/App.jsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">
+				`Click on the ${name_4} and React logos to learn more`
+			</p>
+		</div>
+	);
 }
 
-export default App
+export default App;
