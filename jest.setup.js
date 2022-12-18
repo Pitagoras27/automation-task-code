af5262915fc364556dd58bbf6ddc88e9f5889db9
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2022-12-18 16:22:16 -0600

    fix: Add automate format with prettier and auto fixed with eslint

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,1 @@
-import "setimmediate";
-
+import 'setimmediate';
--- test/App.test.jsx
+++ test/App.test.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "../src/App";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '../src/App';
 
-describe("test for App component", () => {
-  
-  test("should render App", () => {
-    render(<App />)
-  })
-  
-})
+describe('test for App component', () => {
+	test('should render App', () => {
+		render(<App />);
+	});
+});
